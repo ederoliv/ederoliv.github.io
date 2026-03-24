4a4ba87a47eae48e6c6b322144f9288a19e759a2
Eder Oliveira <ederoliveira152@gmail.com>
2026-03-24 17:18:18 -0300

    feat: added AOS effects

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Estilos necessários da biblioteca
 import './index.css'
 import App from './App.tsx'
+
+AOS.init({
+  duration: 1000, 
+  once: true,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
